d16c512052a1652f3adf35c29e27814d0922fab5
Maheen Chowdhury <mahin.mn@live.com>
2022-10-09 09:05:37 +0600

    date of modification changed

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ let text = document.lastModified;
 
 const date = new Date(document.lastModified);
 
-document.getElementById('date_of_modification').innerHTML = "Last updated on " +date
+document.getElementById('date_of_modification').innerHTML = "Last updated on " +text
