258e81cddbdb658be9c3a132dcf393094cb6098e
Maheen Chowdhury <mahin.mn@live.com>
2022-10-12 12:52:18 +0600

    page location added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-let text = document.lastModified;
+const page_location = window.location.href;
+const text = document.lastModified;
 
 const date = new Date(document.lastModified);
 
-document.getElementById('date_of_modification').innerHTML = "Last updated on " +text
+document.getElementById('date_of_modification').innerHTML = "Last updated on " +text+ " at " +page_location;
