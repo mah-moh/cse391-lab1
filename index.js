59a3bab6c561d97a688823c426bccae857d76aad
Maheen Chowdhury <mahin.mn@live.com>
2022-10-14 10:52:44 +0600

    loader added

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ const text = document.lastModified;
 const date = new Date(document.lastModified);
 
 document.getElementById('date_of_modification').innerHTML = "Last updated on " +text+ " at " +page_location;
+
+
+let loader = document.getElementById("preloader");
+window.addEventListener("load", function () {
+  loader.style.display = "none";
+});
